0473d1f43eb481e74b86b934da7f3225e4bd0a17
Melli11 <mingerez@gmail.com>
2022-05-14 14:47:02 -0300

    Empezando Parcial

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,33 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+type Cansancio = Number  
+type Stress = Number  
+type Energia = (Cansancio,Stress)
+
+data Turista = Turista {
+    energia :: (Cansancio,Stress),
+    viajaSolo :: Bool,
+    idiomasQueHaba :: [String]
+}deriving(Show,Eq)
+
+meloso :: Turista
+meloso = Turista (4,10) False ["Castellano"]
+
+sasuke = Turista (100,50) True ["Japones","Español"]
+bajarCansancioEn' unTurista = subtract.fst.energia --ver correccion    
+
+bajarCansancioEn :: Number -> Energia -> Energia
+bajarCansancioEn  cantidad (cansancio,stress) =  (cansancio - cantidad, stress)
+
+bajarStressEn :: Number -> Energia -> Energia
+bajarStressEn cantidad (cansancio,stress) = (cansancio, stress - cantidad)
+
+irAlaPlaya :: Turista -> Turista
+irAlaPlaya unTurista 
+    | viajaSolo unTurista = unTurista {energia =  bajarCansancioEn 5 (energia unTurista)}
+    | otherwise = unTurista {energia = bajarStressEn 1 (energia unTurista)}
+
+
+
+-- apreciarAlgunElementoDelPaisaje elemento unTurista =  
--- src/Spec.hs
+++ src/Spec.hs
@@ -6,6 +6,8 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+    it "Sea un turista como Sasuke que viaja solo, entonces bajará su cansancio en 5 unidades" $ do
+      irAlaPlaya (Turista (100,50) True ["Japones","Español"]) `shouldBe` Turista (95,50) True ["Japones","Español"]
+    it "Sea un turista meloso que no viaja solo, entonces bajará su stress en 1 unidad" $ do
+      irAlaPlaya (Turista (4,10) False ["Castellano"]) `shouldBe` Turista (4,10) False ["Castellano"]
 

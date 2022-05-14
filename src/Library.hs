f59d95dc9186180b58efb6f3b8cc45f15f514583
Melli11 <mingerez@gmail.com>
2022-05-14 15:46:04 -0300

    1) Excursiones Ok

--- src/Library.hs
+++ src/Library.hs
@@ -3,14 +3,14 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
-type Cansancio = Number  
-type Stress = Number  
+type Cansancio = Number
+type Stress = Number
 type Energia = (Cansancio,Stress)
 
 data Turista = Turista {
     energia :: (Cansancio,Stress),
     viajaSolo :: Bool,
-    idiomasQueHaba :: [String]
+    idiomasQueHabla :: [String]
 }deriving(Show,Eq)
 
 meloso :: Turista
@@ -25,11 +25,41 @@ bajarCansancioEn  cantidad (cansancio,stress) =  (cansancio - cantidad, stress)
 bajarStressEn :: Number -> Energia -> Energia
 bajarStressEn cantidad (cansancio,stress) = (cansancio, stress - cantidad)
 
+
 irAlaPlaya :: Turista -> Turista
-irAlaPlaya unTurista 
+irAlaPlaya unTurista
     | viajaSolo unTurista = unTurista {energia =  bajarCansancioEn 5 (energia unTurista)}
     | otherwise = unTurista {energia = bajarStressEn 1 (energia unTurista)}
 
 
+apreciarAlgunElementoDelPaisaje :: String -> Turista -> Turista
+apreciarAlgunElementoDelPaisaje elemento unTurista = unTurista {energia = bajarStressEn (length elemento) (energia unTurista)}
 
--- apreciarAlgunElementoDelPaisaje elemento unTurista =  
+
+salirAHablarUnIdioma :: String -> Turista -> Turista
+salirAHablarUnIdioma unIdioma (Turista energia _ idiomasQueSabe) = Turista energia True (unIdioma:idiomasQueSabe)
+
+caminar :: Number -> Turista -> Turista
+caminar cantidadDeMinutos unTurista = unTurista {energia = afectarSegunTiempo cantidadDeMinutos (energia unTurista) }
+
+afectarSegunTiempo :: Number -> (Number, Number) -> (Number, Number)
+afectarSegunTiempo tiempo (cansancio,stress) =  (cansancio + tiempo/4,stress - tiempo/4)
+
+data NiveldeLaMarea = Fuerte | Moderada | Tranquila
+
+-- paseoEnBarco ::  NiveldeLaMarea -> Turista -> Turista
+paseoEnBarco :: NiveldeLaMarea -> Turista -> Turista
+paseoEnBarco Tranquila unTurista = caminar 10 . apreciarAlgunElementoDelPaisaje "mar" $ salirAHablarUnIdioma "aleman" unTurista 
+paseoEnBarco Fuerte (Turista (cansancio,stress) conCompañia idioma )  = Turista (cansancio + 10 ,stress + 6) conCompañia idioma
+paseoEnBarco Moderada unTurista = unTurista 
+
+
+-- Crear un modelo para los turistas y crear los siguientes tres ejemplos:
+-- Ana: está acompañada, sin cansancio, tiene 21 de stress y habla español.
+ana = Turista (0,21) True ["Español"]
+
+-- Beto y Cathi, que hablan alemán, viajan solos, y Cathi además habla catalán. Ambos tienen 15
+-- unidades de cansancio y stress.
+beto = Turista (15,15) False ["Aleman"]
+cathi = Turista (15,15) False ["Aleman","Catalan"]
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -7,7 +7,7 @@ correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "Sea un turista como Sasuke que viaja solo, entonces bajará su cansancio en 5 unidades" $ do
-      irAlaPlaya (Turista (100,50) True ["Japones","Español"]) `shouldBe` Turista (95,50) True ["Japones","Español"]
+      irAlaPlaya (Turista (100,50) True ["Japones","Español"]) `shouldBe` Turista (100-5,50) True ["Japones","Español"]
     it "Sea un turista meloso que no viaja solo, entonces bajará su stress en 1 unidad" $ do
-      irAlaPlaya (Turista (4,10) False ["Castellano"]) `shouldBe` Turista (4,10) False ["Castellano"]
+      irAlaPlaya (Turista (4,10) False ["Castellano"]) `shouldBe` Turista (4,10-1) False ["Castellano"]
 

b01a2dc4cea4a65a49126a54ab2f2d0dd30558e1
Melli11 <mingerez@gmail.com>
2022-05-14 19:54:03 -0300

    Correcciones y tests

--- src/Library.hs
+++ src/Library.hs
@@ -5,61 +5,103 @@ doble :: Number -> Number
 doble numero = numero + numero
 type Cansancio = Number
 type Stress = Number
-type Energia = (Cansancio,Stress)
+-- type Energia = (Cansancio,Stress)
 
 data Turista = Turista {
-    energia :: (Cansancio,Stress),
+    cansancio :: Number,
+    stress :: Number,
     viajaSolo :: Bool,
     idiomasQueHabla :: [String]
 }deriving(Show,Eq)
 
+-- FUNCIONES AUXILIARES
+modificarStressEn :: Number -> Turista -> Turista
+-- modificarStressEn cantidad (Turista (cansancio,stress) compania idiomas ) = Turista (cansancio,stress + cantidad) compania idiomas --Para la version con tupla energia
+modificarStressEn cantidad (Turista cansancio stress compania idiomas ) = Turista cansancio (stress + cantidad) compania idiomas
+modificarCansancioEn :: Number -> Turista -> Turista
+-- modificarCansancioEn cantidad (Turista (cansancio,stress) compania idiomas ) = Turista (cansancio + cantidad,stress) compania idiomas --Para la version con tupla energia
+modificarCansancioEn cantidad (Turista cansancio stress compania idiomas ) = Turista (cansancio + cantidad) stress  compania idiomas
+
+
+-- modificarCansancioEn cantidad 
+
 meloso :: Turista
-meloso = Turista (4,10) False ["Castellano"]
+meloso = Turista 4 10 False ["Castellano"]
 
-sasuke = Turista (100,50) True ["Japones","Español"]
-bajarCansancioEn' unTurista = subtract.fst.energia --ver correccion    
+sasuke = Turista 100 50 True ["Japones","Español"]
 
-bajarCansancioEn :: Number -> Energia -> Energia
-bajarCansancioEn  cantidad (cansancio,stress) =  (cansancio - cantidad, stress)
 
-bajarStressEn :: Number -> Energia -> Energia
-bajarStressEn cantidad (cansancio,stress) = (cansancio, stress - cantidad)
-
+-- irAlaPlaya :: Turista -> Turista
+-- irAlaPlaya unTurista
+--     | viajaSolo unTurista = unTurista {energia =  bajarCansancioEn 5 (energia unTurista)}
+--     | otherwise = unTurista {energia = bajarStressEn 1 (energia unTurista)}
 
 irAlaPlaya :: Turista -> Turista
 irAlaPlaya unTurista
-    | viajaSolo unTurista = unTurista {energia =  bajarCansancioEn 5 (energia unTurista)}
-    | otherwise = unTurista {energia = bajarStressEn 1 (energia unTurista)}
-
+    | viajaSolo unTurista = modificarCansancioEn (-5) unTurista
+    | otherwise = modificarStressEn (-1) unTurista
 
 apreciarAlgunElementoDelPaisaje :: String -> Turista -> Turista
-apreciarAlgunElementoDelPaisaje elemento unTurista = unTurista {energia = bajarStressEn (length elemento) (energia unTurista)}
+-- apreciarAlgunElementoDelPaisaje elemento unTurista = unTurista {energia = bajarStressEn (length elemento) (energia unTurista)}
 
+-- apreciarAlgunElementoDelPaisaje  = modificarStressEn.((-1)*length)
+apreciarAlgunElementoDelPaisaje elemento = modificarStressEn ( (-1)* length elemento)
 
 salirAHablarUnIdioma :: String -> Turista -> Turista
-salirAHablarUnIdioma unIdioma (Turista energia _ idiomasQueSabe) = Turista energia True (unIdioma:idiomasQueSabe)
+salirAHablarUnIdioma unIdioma (Turista cansancio stress _ idiomasQueSabe) = Turista cansancio stress True (unIdioma:idiomasQueSabe)
+
+-- caminar :: Number -> Turista -> Turista
+-- caminar cantidadDeMinutos unTurista = unTurista {energia = afectarSegunTiempo cantidadDeMinutos (energia unTurista) }
 
 caminar :: Number -> Turista -> Turista
-caminar cantidadDeMinutos unTurista = unTurista {energia = afectarSegunTiempo cantidadDeMinutos (energia unTurista) }
+caminar cantidadDeMinutos  = modificarCansancioEn (div cantidadDeMinutos 4) . modificarStressEn (div (-cantidadDeMinutos) 4)  
 
-afectarSegunTiempo :: Number -> (Number, Number) -> (Number, Number)
-afectarSegunTiempo tiempo (cansancio,stress) =  (cansancio + tiempo/4,stress - tiempo/4)
+-- afectarSegunTiempo :: Number -> (Number, Number) -> (Number, Number)
+-- afectarSegunTiempo tiempo (cansancio,stress) =  (cansancio + tiempo/4,stress - tiempo/4)
 
 data NiveldeLaMarea = Fuerte | Moderada | Tranquila
 
 -- paseoEnBarco ::  NiveldeLaMarea -> Turista -> Turista
 paseoEnBarco :: NiveldeLaMarea -> Turista -> Turista
-paseoEnBarco Tranquila unTurista = caminar 10 . apreciarAlgunElementoDelPaisaje "mar" $ salirAHablarUnIdioma "aleman" unTurista 
-paseoEnBarco Fuerte (Turista (cansancio,stress) conCompañia idioma )  = Turista (cansancio + 10 ,stress + 6) conCompañia idioma
-paseoEnBarco Moderada unTurista = unTurista 
+paseoEnBarco Fuerte unTurista = (modificarStressEn 6 . modificarCansancioEn 10) unTurista
+paseoEnBarco Tranquila unTurista = caminar 10 . apreciarAlgunElementoDelPaisaje "mar" $ salirAHablarUnIdioma "aleman" unTurista
+paseoEnBarco Moderada unTurista = unTurista
 
 
 -- Crear un modelo para los turistas y crear los siguientes tres ejemplos:
 -- Ana: está acompañada, sin cansancio, tiene 21 de stress y habla español.
-ana = Turista (0,21) True ["Español"]
+ana = Turista 0 21 True ["Español"]
 
 -- Beto y Cathi, que hablan alemán, viajan solos, y Cathi además habla catalán. Ambos tienen 15
 -- unidades de cansancio y stress.
-beto = Turista (15,15) False ["Aleman"]
-cathi = Turista (15,15) False ["Aleman","Catalan"]
+beto = Turista 15 15 False ["Aleman"]
+cathi = Turista 15 15 False ["Aleman","Catalan"]
 
+-- 2. Modelar las excursiones anteriores de forma tal que para agregar una excursión al sistema no haga
+-- falta modificar las funciones existentes. Además:
+
+type Excursion = Turista -> Turista
+
+-- a. Hacer que un turista haga una excursión. Al hacer una excursión, el turista además de sufrir los
+-- efectos propios de la excursión, reduce en un 10% su stress.
+
+
+stressDelTuristaEn10Menos :: Turista -> Turista
+stressDelTuristaEn10Menos  (Turista cansancio stress compania idiomas ) =  Turista cansancio (stress*0.90) compania idiomas
+
+hacerUnaExcursion :: Turista -> Excursion -> Turista 
+hacerUnaExcursion  unTurista =  stressDelTuristaEn10Menos .  aplicarExcursion unTurista 
+
+aplicarExcursion unTurista excursion = excursion unTurista
+
+
+
+-- b. Dada la función
+deltaSegun :: (a -> Number) -> a -> a -> Number
+deltaSegun f algo1 algo2 = f algo1 - f algo2
+
+-- deltaExcursionSegun indice unTurista excursion = 
+
+-- Definir la función deltaExcursionSegun que a partir de un índice, un turista y una excursión
+-- determine cuánto varió dicho índice después de que el turista haya hecho la excursión.
+-- Llamamos índice a cualquier función que devuelva un número a partir de un turista
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,9 +5,29 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
+  describe "irAlaPlaya" $ do
     it "Sea un turista como Sasuke que viaja solo, entonces bajará su cansancio en 5 unidades" $ do
-      irAlaPlaya (Turista (100,50) True ["Japones","Español"]) `shouldBe` Turista (100-5,50) True ["Japones","Español"]
+      irAlaPlaya (Turista 100 50 True ["Japones","Español"]) `shouldBe` Turista (100-5) 50 True ["Japones","Español"]
     it "Sea un turista meloso que no viaja solo, entonces bajará su stress en 1 unidad" $ do
-      irAlaPlaya (Turista (4,10) False ["Castellano"]) `shouldBe` Turista (4,10-1) False ["Castellano"]
+      irAlaPlaya (Turista 4 10 False ["Castellano"]) `shouldBe` Turista 4 (10-1) False ["Castellano"]
+  describe "salirAHablarUnIdioma" $ do
+    it "Sea un turista que sale a hablar un idioma entonces el turista aprende el idioma y ademas termina acompañado" $ do
+      salirAHablarUnIdioma "Aleman" (Turista 4 10 False ["Castellano"]) `shouldBe` Turista 4 10 True ["Aleman","Castellano"]
+  describe "apreciarAlgunElementoDelPaisaje" $ do
+    it "Sea un turista que contempla el mar, entonces rudicirá su nivel de stress en 3 unidades" $ do
+      apreciarAlgunElementoDelPaisaje "mar" (Turista 4 10 False ["Castellano"]) `shouldBe` Turista 4 (10-3) False ["Castellano"]
+    it "Sea un turista que contempla la nada misma, entonces rudicirá su nivel de stress en 0 unidades" $ do
+      apreciarAlgunElementoDelPaisaje "" (Turista 4 10 False ["Castellano"]) `shouldBe` Turista 4 10 False ["Castellano"]
+  describe "caminar" $ do
+    it "Sea un turista que camina una cantidad X de minutos , aumentará su cansancio pero reducirá su stress " $ do
+      caminar 20 (Turista 4 10 False ["Castellano"]) `shouldBe` Turista (4+5) (10-5) False ["Castellano"]
+    it "Sea un turista vago que no camina  entonces no cambiará su cansancio ni su stress " $ do
+      caminar 0 sasuke `shouldBe` sasuke
+  describe "paseo en barco" $ do
+    it "Si un turista realiza un paseo con la marea FUERTE en barco aumentará su stress en 6 unidades y el cansancio en 10 " $ do
+      paseoEnBarco  Fuerte (Turista 4 10 False ["Castellano"]) `shouldBe` Turista (4+10) (10+6) False ["Castellano"]
+    it "Si un turista realiza un paseo con la marea MODERADA , no se verá afectado " $ do
+      paseoEnBarco  Moderada sasuke `shouldBe` sasuke
+    it "Si un turista realiza un paseo con la marea TRANQUILA, el turista hace sociales, camina 10', aprecia la vista del mar y sale a hablar aleman" $ do
+      paseoEnBarco  Tranquila (Turista 4 10 False ["Castellano"]) `shouldBe` Turista 6 4 True ["aleman","Castellano"]
 
